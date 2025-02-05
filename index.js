b09b1ed618b0837492785681e4b678d230768ea1
Wei-Chung Cheng <weichung.cheng@galileo.edu>
2025-02-05 08:54:34 -0600

    7th commit. Documented the code.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Get nodes from the document: the input form and the divs where the results will be displayed.
 const form = document.getElementById("weather-form");
 const resultDiv = document.getElementById("weatherResult");
 const chart = document.getElementById("barChart");
@@ -5,6 +6,7 @@ const chart = document.getElementById("barChart");
 form.addEventListener("submit", async (event) => {
     event.preventDefault();
 
+    // Given that the input is a list of cities separated by commas, the input is split by the commas and stored inside an array.
     const inputs = document.getElementById("citiesInput").value.split(',');
     const cities = [];
     const dataset = [];
@@ -12,15 +14,19 @@ form.addEventListener("submit", async (event) => {
     resultDiv.innerHTML = "";
     chart.innerHTML = "";
 
+    // Check that inputs were given.
     if (!cities) {
         resultDiv.innerHTML = `<div class="alert alert-danger">Please enter a city name.</div>`;
         return;
     }
+
+    // Fetch data from the Current Weather API for all the cities and store the responses inside an array.
     const apiKey = "1e988bb69fe10e044d104c082b3616f2";
     const responses = cities.map(async (city) => await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
     ));
 
+    // Process all the API requests concurrently. For each city: checks if it is found, retrieves the data, and creates a responsive card with the weather information.
     Promise.all(responses).then(async (responses) => {
         for (const response of responses) {
             if (!response.ok) {
@@ -42,6 +48,7 @@ form.addEventListener("submit", async (event) => {
             `;
         }
         
+        // Generates a comparative bar chart of the cities' temperatures.
         chart.innerHTML = '<canvas id="weatherChart"></canvas>'
         new Chart(document.getElementById("weatherChart"), {
             type: 'bar',
@@ -63,6 +70,7 @@ form.addEventListener("submit", async (event) => {
                 }
             }
         });
+        // If any error ocurred, display the error message.
     }).catch( error => {
         resultDiv.innerHTML = `<div class="alert alert-danger">${error.message}</div>`;
     });
